c553e14a42023263da38231be63b90890aa90f08
diptimayee beura <swadhin@yellowcloud.in>
2020-10-29 00:44:03 +0530

    adding abc.java

--- src/main/java/com/OrangeHrm/qa/Base/abc.java
+++ src/main/java/com/OrangeHrm/qa/Base/abc.java
@@ -1,6 +1,8 @@
 package com.OrangeHrm.qa.Base;
 
 public class abc {
+	
+	//change is done.
 
 	public static void main(String[] args) {
 		 System.out.println("file path is >>>>> "+System.getProperty("user.dir"));
